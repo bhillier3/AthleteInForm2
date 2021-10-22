c89ca9db56f6501f1a0be02e7be93a6ad70e5634
bhillier3 <bhillier3@gatech.edu>
2021-10-22 14:45:41 -0500

    renamed user to athlete in server and setup front-end for connection

--- server/models/Athlete.js
+++ server/models/Athlete.js
@@ -2,11 +2,11 @@
 
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const AthleteSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
   email: { type: String, required: true },
   forms: { type: Array, default: [] }
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = Athlete = mongoose.model('athlete', AthleteSchema);
